18a91c4dc67d74e47d37dc6e321348b026798548
Georgian Nechifor <georgian.nechifor@bitstone.eu>
2022-04-25 20:09:19 +0300

    feat: display loader on change router

--- src/components/misc/Loader.jsx
+++ src/components/misc/Loader.jsx
@@ -3,7 +3,7 @@ import {bool, elementType, object, oneOfType} from 'prop-types';
 const Loader = ({isLoading, children}) => (
   <>
     { isLoading ? (
-      <div className="z-20 absolute bg-black bg-opacity-30 flex items-center justify-center h-screen w-screen">
+      <div className="z-20 absolute top-0 left-0   bg-black bg-opacity-30 flex items-center justify-center h-screen w-screen">
         <span className="z-30 animate-spin h-10 w-10 rounded-full border-4 border-t-blue-500" />
       </div>
     ) : null
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,11 @@
 import {object, elementType, string} from 'prop-types';
-import {useRouter} from 'next/router';
+import {useRouter, Router} from 'next/router';
 
 import '../styles/globals.scss';
-import {Layout, AdminLayout} from 'components';
+import {Layout, AdminLayout, Loader} from 'components';
 import {PUBLIC_PATHS} from 'constants/index';
 import {SWRConfig} from 'swr';
+import {useEffect, useState} from 'react';
 
 const PageLayout = ({children, route}) => {
   if (route && route.includes('/admin-config')) {
@@ -25,17 +26,38 @@ const PageLayout = ({children, route}) => {
 
 const App = ({Component, pageProps}) => {
   const router = useRouter();
+  const [loaderScreen, setLoaderScreen] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setLoaderScreen(true);
+    const handleStop = () => setLoaderScreen(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
 
   return (
-    <PageLayout route={router.asPath}>
-      <SWRConfig value={{
-        fetcher : endpoint => fetch(`${process.env.ORIGIN_URL}/api` + endpoint).then(res => res.json())
-          .then(res => res.data)
-      }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
-    </PageLayout>
+    <Loader isLoading={loaderScreen}>
+      <PageLayout route={router.asPath}>
+        <SWRConfig value={{
+          fetcher : endpoint => fetch(`${process.env.ORIGIN_URL}/api` + endpoint).then(res => res.json())
+            .then(res => res.data)
+        }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
+      </PageLayout>
+    </Loader>
 
   );
 };
